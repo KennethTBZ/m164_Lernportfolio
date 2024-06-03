740173aa4069d106c9f33e034c6f5592979669d1
KennethTBZ <kenneth.dang@edu.tbz.ch>
2024-06-03 16:02:37 +0200

    Changed text casing

--- Tag3/Repetition DML üK106/Update_delete_alter_drop_Auftrag.sql
+++ Tag3/Repetition DML üK106/Update_delete_alter_drop_Auftrag.sql
@@ -6,18 +6,18 @@ UPDATE dvd_sammlung
 SET laenge_minuten = 120
 WHERE film = 'Angst' AND laenge_minuten = 92;
 
-RENAME TABLE dvd_sammlung TO bluray_sammlung;
+RENAME TABLE dvd_sammlung TO bluray_Sammlung;
 
-ALTER TABLE bluray_sammlung
+ALTER TABLE bluray_Sammlung
 ADD COLUMN preis DECIMAL(10, 2);
 
-DELETE FROM bluray_sammlung
+DELETE FROM bluray_Sammlung
 WHERE film = 'Angriff auf Rom' AND regisseur = 'Steven Burghofer';
 
-ALTER TABLE bluray_sammlung
+ALTER TABLE bluray_Sammlung
 CHANGE COLUMN film kinofilme VARCHAR(255);
 
-ALTER TABLE bluray_sammlung
+ALTER TABLE bluray_Sammlung
 DROP COLUMN nummer;
 
-DROP TABLE IF EXISTS bluray_sammlung;
+DROP TABLE IF EXISTS bluray_Sammlung;
